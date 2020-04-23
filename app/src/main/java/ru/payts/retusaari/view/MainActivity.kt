525540fa5a8c0edc62b976784ab799e5f68fe308
Michael V. Gritsenko <payterms@mail.ru>
2020-04-23 16:52:17 +0300

    Button implemented via setOnClickListener (removed from layout)

--- app/src/main/java/ru/payts/retusaari/view/MainActivity.kt
+++ app/src/main/java/ru/payts/retusaari/view/MainActivity.kt
@@ -1,24 +1,28 @@
 package ru.payts.retusaari.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.payts.retusaari.R
 import ru.payts.retusaari.viewmodel.MainViewModel
-import androidx.activity.viewModels
 
 
 class MainActivity : AppCompatActivity() {
     //Use the 'by viewModels()' Kotlin property delegate or
     // ViewModelProvider.ViewModelProvider(ViewModelStoreOwner), passing in the activity.
-    private val viewModel: MainViewModel by viewModels()
+    //private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         // deprecated
         // viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
@@ -27,9 +31,11 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
         })
 
-    }
+        val button: Button = findViewById(R.id.magic_button)
+        button.setOnClickListener {
+            // Code here executes on main thread after user presses button
+            Toast.makeText(this, "So gently!", Toast.LENGTH_SHORT).show()
+        }
 
-    fun buttonTouched(view: View) {
-        Toast.makeText(this, "So gently!", Toast.LENGTH_SHORT).show()
     }
 }
